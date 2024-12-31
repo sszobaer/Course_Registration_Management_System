740f0d6b8a66c9f400cee45cc70903d33af4bf23
sszobaer <22-49415-3@student.aiub.edu>
2025-01-01 00:44:01 +0600

    Add project files.

---
+++ CRMS/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace CRMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.AdministratorLbl = new System.Windows.Forms.Label();
+            this.DepartmentsLbl = new System.Windows.Forms.Label();
+            this.ClassscheduleLbl = new System.Windows.Forms.Label();
+            this.FacultyLbl = new System.Windows.Forms.Label();
+            this.PaymentsLbl = new System.Windows.Forms.Label();
+            this.RegistrationLbl = new System.Windows.Forms.Label();
+            this.CoursesLbl = new System.Windows.Forms.Label();
+            this.studentLbl = new System.Windows.Forms.Label();
+            this.DashboardLbl = new System.Windows.Forms.Label();
+            this.HeroLbl = new System.Windows.Forms.Label();
+            this.HomeLbl = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Controls.Add(this.AdministratorLbl);
+            this.panel1.Controls.Add(this.DepartmentsLbl);
+            this.panel1.Controls.Add(this.ClassscheduleLbl);
+            this.panel1.Controls.Add(this.FacultyLbl);
+            this.panel1.Controls.Add(this.PaymentsLbl);
+            this.panel1.Controls.Add(this.RegistrationLbl);
+            this.panel1.Controls.Add(this.CoursesLbl);
+            this.panel1.Controls.Add(this.studentLbl);
+            this.panel1.Controls.Add(this.DashboardLbl);
+            this.panel1.Controls.Add(this.HeroLbl);
+            this.panel1.Controls.Add(this.HomeLbl);
+            this.panel1.Location = new System.Drawing.Point(-3, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 712);
+            this.panel1.TabIndex = 5;
+            // 
+            // AdministratorLbl
+            // 
+            this.AdministratorLbl.AutoSize = true;
+            this.AdministratorLbl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AdministratorLbl.Font = new System.Drawing.Font("Montserrat ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AdministratorLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.AdministratorLbl.Location = new System.Drawing.Point(14, 615);
+            this.AdministratorLbl.Name = "AdministratorLbl";
+            this.AdministratorLbl.Size = new System.Drawing.Size(197, 33);
+            this.AdministratorLbl.TabIndex = 12;
+            this.AdministratorLbl.Text = "Administrator";
+            // 
+            // DepartmentsLbl
+            // 
+            this.DepartmentsLbl.AutoSize = true;
+            this.DepartmentsLbl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.DepartmentsLbl.Font = new System.Drawing.Font("Montserrat ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DepartmentsLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DepartmentsLbl.Location = new System.Drawing.Point(14, 559);
+            this.DepartmentsLbl.Name = "DepartmentsLbl";
+            this.DepartmentsLbl.Size = new System.Drawing.Size(185, 33);
+            this.DepartmentsLbl.TabIndex = 11;
+            this.DepartmentsLbl.Text = "Departments";
+            // 
+            // ClassscheduleLbl
+            // 
+            this.ClassscheduleLbl.AutoSize = true;
+            this.ClassscheduleLbl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ClassscheduleLbl.Font = new System.Drawing.Font("Montserrat ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClassscheduleLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClassscheduleLbl.Location = new System.Drawing.Point(14, 501);
+            this.ClassscheduleLbl.Name = "ClassscheduleLbl";
+            this.ClassscheduleLbl.Size = new System.Drawing.Size(208, 33);
+            this.ClassscheduleLbl.TabIndex = 10;
+            this.ClassscheduleLbl.Text = "Class Schedule";
+            // 
+            // FacultyLbl
+            // 
+            this.FacultyLbl.AutoSize = true;
+            this.FacultyLbl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.FacultyLbl.Font = new System.Drawing.Font("Montserrat ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FacultyLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FacultyLbl.Location = new System.Drawing.Point(14, 447);
+            this.FacultyLbl.Name = "FacultyLbl";
+            this.FacultyLbl.Size = new System.Drawing.Size(111, 33);
+            this.FacultyLbl.TabIndex = 9;
+            this.FacultyLbl.Text = "Faculty";
+            // 
+            // PaymentsLbl
+            // 
+            this.PaymentsLbl.AutoSize = true;
+            this.PaymentsLbl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PaymentsLbl.Font = new System.Drawing.Font("Montserrat ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PaymentsLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PaymentsLbl.Location = new System.Drawing.Point(14, 393);
+            this.PaymentsLbl.Name = "PaymentsLbl";
+            this.PaymentsLbl.Size = new System.Drawing.Size(144, 33);
+            this.PaymentsLbl.TabIndex = 8;
+            this.PaymentsLbl.Text = "Payments";
+            // 
+            // RegistrationLbl
+            // 
+            this.RegistrationLbl.AutoSize = true;
+            this.RegistrationLbl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RegistrationLbl.Font = new System.Drawing.Font("Montserrat ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegistrationLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.RegistrationLbl.Location = new System.Drawing.Point(14, 343);
+            this.RegistrationLbl.Name = "RegistrationLbl";
+            this.RegistrationLbl.Size = new System.Drawing.Size(175, 33);
+            this.RegistrationLbl.TabIndex = 7;
+            this.RegistrationLbl.Text = "Registration";
+            // 
+            // CoursesLbl
+            // 
+            this.CoursesLbl.AutoSize = true;
+            this.CoursesLbl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CoursesLbl.Font = new System.Drawing.Font("Montserrat ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CoursesLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CoursesLbl.Location = new System.Drawing.Point(14, 290);
+            this.CoursesLbl.Name = "CoursesLbl";
+            this.CoursesLbl.Size = new System.Drawing.Size(118, 33);
+            this.CoursesLbl.TabIndex = 6;
+            this.CoursesLbl.Text = "Courses";
+            // 
+            // studentLbl
+            // 
+            this.studentLbl.AutoSize = true;
+            this.studentLbl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.studentLbl.Font = new System.Drawing.Font("Montserrat ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.studentLbl.Location = new System.Drawing.Point(14, 236);
+            this.studentLbl.Name = "studentLbl";
+            this.studentLbl.Size = new System.Drawing.Size(132, 33);
+            this.studentLbl.TabIndex = 5;
+            this.studentLbl.Text = "Students";
+            // 
+            // DashboardLbl
+            // 
+            this.DashboardLbl.AutoSize = true;
+            this.DashboardLbl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.DashboardLbl.Font = new System.Drawing.Font("Montserrat ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DashboardLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DashboardLbl.Location = new System.Drawing.Point(14, 186);
+            this.DashboardLbl.Name = "DashboardLbl";
+            this.DashboardLbl.Size = new System.Drawing.Size(156, 33);
+            this.DashboardLbl.TabIndex = 4;
+            this.DashboardLbl.Text = "Dashboard";
+            // 
+            // HeroLbl
+            // 
+            this.HeroLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.HeroLbl.AutoSize = true;
+            this.HeroLbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HeroLbl.Font = new System.Drawing.Font("Luckiest Guy", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.HeroLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.HeroLbl.Location = new System.Drawing.Point(30, 9);
+            this.HeroLbl.Name = "HeroLbl";
+            this.HeroLbl.Size = new System.Drawing.Size(158, 98);
+            this.HeroLbl.TabIndex = 3;
+            this.HeroLbl.Text = "CRMS\r\nPORTAL\r\n";
+            this.HeroLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HomeLbl
+            // 
+            this.HomeLbl.AutoSize = true;
+            this.HomeLbl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.HomeLbl.Font = new System.Drawing.Font("Montserrat ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.HomeLbl.Location = new System.Drawing.Point(14, 135);
+            this.HomeLbl.Name = "HomeLbl";
+            this.HomeLbl.Size = new System.Drawing.Size(90, 33);
+            this.HomeLbl.TabIndex = 2;
+            this.HomeLbl.Text = "Home";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 702);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label AdministratorLbl;
+        private System.Windows.Forms.Label DepartmentsLbl;
+        private System.Windows.Forms.Label ClassscheduleLbl;
+        private System.Windows.Forms.Label FacultyLbl;
+        private System.Windows.Forms.Label PaymentsLbl;
+        private System.Windows.Forms.Label RegistrationLbl;
+        private System.Windows.Forms.Label CoursesLbl;
+        private System.Windows.Forms.Label studentLbl;
+        private System.Windows.Forms.Label DashboardLbl;
+        private System.Windows.Forms.Label HeroLbl;
+        private System.Windows.Forms.Label HomeLbl;
+    }
+}
+
