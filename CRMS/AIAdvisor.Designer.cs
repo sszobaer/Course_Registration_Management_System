8966da089b4268ae784460199c99a563dd58af70
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2025-01-31 04:47:05 +0600

    Connected with the Olloma server

---
+++ CRMS/AIAdvisor.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CRMS
+{
+    partial class AIAdvisor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HeroLbl = new System.Windows.Forms.Label();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtbChat = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // HeroLbl
+            // 
+            this.HeroLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.HeroLbl.AutoSize = true;
+            this.HeroLbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HeroLbl.Font = new System.Drawing.Font("Luckiest Guy", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.HeroLbl.ForeColor = System.Drawing.Color.Teal;
+            this.HeroLbl.Location = new System.Drawing.Point(220, 9);
+            this.HeroLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.HeroLbl.Name = "HeroLbl";
+            this.HeroLbl.Size = new System.Drawing.Size(345, 33);
+            this.HeroLbl.TabIndex = 4;
+            this.HeroLbl.Text = "Artifitial Smart Advisor";
+            this.HeroLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtUserInput
+            // 
+            this.txtUserInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUserInput.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserInput.Location = new System.Drawing.Point(66, 106);
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(591, 39);
+            this.txtUserInput.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Write your message";
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.BackColor = System.Drawing.Color.Teal;
+            this.sendBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sendBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sendBtn.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.sendBtn.Location = new System.Drawing.Point(663, 105);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(125, 40);
+            this.sendBtn.TabIndex = 10;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = false;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 22);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Smart Advisor";
+            // 
+            // rtbChat
+            // 
+            this.rtbChat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbChat.Location = new System.Drawing.Point(66, 189);
+            this.rtbChat.Name = "rtbChat";
+            this.rtbChat.Size = new System.Drawing.Size(722, 233);
+            this.rtbChat.TabIndex = 13;
+            this.rtbChat.Text = "";
+            // 
+            // AIAdvisor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbChat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUserInput);
+            this.Controls.Add(this.HeroLbl);
+            this.Name = "AIAdvisor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AIAdvisor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HeroLbl;
+        private System.Windows.Forms.TextBox txtUserInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtbChat;
+    }
+}
---
+++ CRMS/AIAdvisor.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRMS
+{
+    public partial class AIAdvisor : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public AIAdvisor()
+        {
+            InitializeComponent();
+        }
+
+        private async void sendBtn_Click(object sender, EventArgs e)
+        {
+            string userMessage = txtUserInput.Text;
+
+            if (string.IsNullOrWhiteSpace(userMessage))
+            {
+                MessageBox.Show("Please enter a message.");
+                return;
+            }
+
+            // Append user's message to the RichTextBox
+            AppendToRichTextBox($"You: {userMessage}", Color.Black, FontStyle.Bold);
+
+            // Fetch the response from the chatbot (including course suggestions if relevant)
+            string botResponse = await GetChatbotResponse(userMessage);
+
+            // Format the response
+            string formattedResponse = FormatResponse(botResponse);
+
+            // Append chatbot's response to the RichTextBox
+            AppendToRichTextBox($"Bot:", Color.DarkGreen, FontStyle.Bold);
+            AppendToRichTextBox(formattedResponse, Color.DarkBlue, FontStyle.Regular);
+
+            // Clear input field
+            txtUserInput.Clear();
+        }
+
+        private async Task<string> GetChatbotResponse(string message)
+        {
+            try
+            {
+                var requestBody = new { message = message };
+                string json = JsonConvert.SerializeObject(requestBody);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                // Send POST request to Flask API
+                HttpResponseMessage response = await client.PostAsync("http://127.0.0.1:5000/chat", content);
+                response.EnsureSuccessStatusCode();
+
+                // Read and return response from API
+                string responseBody = await response.Content.ReadAsStringAsync();
+                dynamic result = JsonConvert.DeserializeObject(responseBody);
+
+                return result.response.ToString();
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+
+        // Helper Method: Append Text to RichTextBox with Formatting
+        private void AppendToRichTextBox(string text, Color color, FontStyle fontStyle)
+        {
+            rtbChat.SelectionStart = rtbChat.TextLength;
+            rtbChat.SelectionLength = 0;
+
+            rtbChat.SelectionColor = color;
+            rtbChat.SelectionFont = new Font("Arial", 10, fontStyle);
+
+            rtbChat.AppendText(text + Environment.NewLine);
+
+            rtbChat.ScrollToCaret();
+        }
+
+        // Helper Method: Format the Response
+        private string FormatResponse(string response)
+        {
+            // Replace escaped newlines with actual newlines
+            string formatted = response.Replace("\\n", Environment.NewLine);
+
+            // Optionally wrap code blocks with markers for better visual separation
+            if (response.Contains("{") && response.Contains("}"))
+            {
+                formatted = $"Code Snippet:{Environment.NewLine}{formatted}{Environment.NewLine}";
+            }
+
+            return formatted;
+        }
+    }
+}
